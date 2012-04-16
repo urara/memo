0ee6f77524c6bcb9e021a047a9465cb66d726d42
urara036 <oura036@gmail.com>
2012-04-16 22:49:27 +0900

    error but cun ran ap server

--- Gemfile
+++ Gemfile
@@ -2,12 +2,15 @@ source 'https://rubygems.org'
 
 gem 'rails', '3.2.2'
 
+gem 'devise'
+
 # Bundle edge Rails instead:
 # gem 'rails', :git => 'git://github.com/rails/rails.git'
 
 group :development, :test do
   gem 'sqlite3'
 end
+
 group :production do
   gem 'pg', '0.12.2'
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,6 @@
 Memotest::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
-
+  config.action_mailer.default_url_options = { :host => 'furious-river-2377.herokuapp.com' }
   # Code is not reloaded between requests
   config.cache_classes = true
 
